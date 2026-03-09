24060dc7eeaf6b4ee4dc1432d895c89b8ae75564
daivengalan <daivenclark@gmail.com>
2026-03-09 22:11:20 +0800

    rephrased the first line, instead of two numbers, the code asks now for base and exponent number to be more specific and clear, now the plan is executed and working

---
+++ prints_first_number_raised_to_second_number_result.py
@@ -0,0 +1,6 @@
+#Ask the user for base and exponent numbers
+base_number = float(input("Enter the base number: "))
+exponent_number = float(input("Enter the exponent: "))
+# Calculate the result
+calculation_result = base_number ** exponent_number
+print(f"{base_number} raised to the power of {exponent_number} is: {calculation_result}")
