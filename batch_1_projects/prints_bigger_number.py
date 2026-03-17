3f54b61117518e64657da88693abedfa61033fc7
daivengalan <daivenclark@gmail.com>
2026-03-17 22:22:57 +0800

    Restoring all files and organized folders

---
+++ batch_1_projects/prints_bigger_number.py
@@ -0,0 +1,7 @@
+#Ask the user for two numbers
+
+#Compare the numbers using an if-else statement
+
+#Another condition if the numbers are exactly the same
+
+#Print the bigger number
---
+++ batch_1_projects/sum_of _ten_numbers.py
@@ -0,0 +1,13 @@
+# Initialize the total sum and the counter
+total_sum = 0.0
+counter = 0
+# Using a while loop to ask for 10 numbers
+while counter < 10:
+    # Adding 1 to the display so the user sees "Number 1", "Number 2", etc.
+    user_input = float(input(f"Enter number {counter + 1}: "))
+    # Add the input to our running total
+    total_sum += user_input
+    # Increment the counter
+    counter += 1
+#Print the sum of 10 num
+print(f"The total sum of the 10 numbers is: {total_sum}")
