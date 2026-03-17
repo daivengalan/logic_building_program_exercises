a9f9efdaafc79cdf3226af881e442a94ae35f9c3
daivengalan <daivenclark@gmail.com>
2026-03-17 22:17:28 +0800

    cleaning to reorganize

--- logic_prog_01_10/prints_product_of_two_numbers.py
+++
@@ -1,7 +0,0 @@
-#Ask the user for two numbers
-first_input_number = float(input("Enter the first number: "))
-second_input_number = float(input("Enter the second number: "))
-#multiply the two numbers through an operator
-product = first_input_number * second_input_number
-#print product
-print(f"The product is {product}")
--- prints_bigger_number.py
+++
@@ -1,7 +0,0 @@
-#Ask the user for two numbers
-
-#Compare the numbers using an if-else statement
-
-#Another condition if the numbers are exactly the same
-
-#Print the bigger number
