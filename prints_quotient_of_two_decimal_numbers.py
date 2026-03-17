22d95bc9f8646a23c7dcc14522d225ba6ae82c4c
daivengalan <daivenclark@gmail.com>
2026-03-17 22:11:21 +0800

    added only the smaller number exercise

--- prints_quotient_of_two_decimal_numbers.py
+++
@@ -1,7 +0,0 @@
-#Ask the user for two numbers, and use float
-first_input_number = float(input("Enter the first number: "))
-second_input_number = float(input("Enter the second number: "))
-#divide the two numbers through an operator
-qoutient = first_input_number / second_input_number
-#print product
-print(f"The qoutient is {qoutient}")
