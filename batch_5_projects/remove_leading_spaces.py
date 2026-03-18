f2d1d1b1b9c1d9f92ea6c880acba5f45fa02eb0d
daivengalan <daivenclark@gmail.com>
2026-03-18 11:49:47 +0800

    used the lstrip method to clean up the input; it specifically targets and removes those leading spaces while keeping the rest of the name intact

---
+++ batch_5_projects/remove_leading_spaces.py
@@ -0,0 +1,4 @@
+user_full_name = input("Enter your full name with leading spaces: ")
+# lstrip() removes spaces only from the left (beginning)
+formatted_name = user_full_name.lstrip()
+print(formatted_name)
