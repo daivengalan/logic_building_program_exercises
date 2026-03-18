3bd9697585ece790209b90b3d3196744d81c1ce3
daivengalan <daivenclark@gmail.com>
2026-03-18 10:49:13 +0800

    plotted the code for getting the difference of two numbers, I just made the same procedure with the first batch (sum of two numbers) but i replaced it with difference

---
+++ batch_2_projects/difference_of_two_numbers.py
@@ -0,0 +1,7 @@
+#Ask the user for two numbers
+first_input_number = float(input("Enter the first number: "))
+second_input_number = float(input("Enter the second number: "))
+#get diff of the two numbers through an operator
+difference = first_input_number - second_input_number
+#print dif
+print(f"The difference is {difference}")
